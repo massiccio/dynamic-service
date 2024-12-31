3b3b7ec367ce18fedb081b60f336a797502b51db
Michele Mazzucco <michele.mazzucco@swisscom.com>
2024-12-31 18:39:26 +0100

    Moving confiuration files - all services are registered in Consul

--- app/main.go
+++ app/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
   "github.com/gin-gonic/gin"
-  "github.com/hashicorp/consul/api"
+  consulapi "github.com/hashicorp/consul/api"
   "log"
   "os"
   "os/signal"
@@ -16,9 +16,9 @@ import (
 )
 
 func deregisterServiceWithConsul(serviceID string) {
-  config := api.DefaultConfig()
+  config := consulapi.DefaultConfig()
   config.Address = os.Getenv("CONSUL_HTTP_ADDR")
-  client, err := api.NewClient(config)
+  client, err := consulapi.NewClient(config)
   if err != nil {
       log.Fatalf("Error creating Consul client: %v", err)
   }
@@ -37,12 +37,8 @@ func getServiceID() string {
   return serviceID
 }
 
-func registerServiceWithConsul(serviceID string) {
+func registerServiceWithConsul(s *service) {
   consulAddr := os.Getenv("CONSUL_HTTP_ADDR")
-  serviceName := os.Getenv("SERVICE_NAME")
-  servicePort := os.Getenv("SERVICE_PORT")
-
-  // serviceID := getServiceId()
   serviceTags := []string{"golang-service"} // You can add more tags for the service
   serviceIP := os.Getenv("SERVICE_IP")  // Optionally pass if known, or discover dynamically
 
@@ -51,28 +47,28 @@ func registerServiceWithConsul(serviceID string) {
   }
 
   // Create a new Consul client
-  config := api.DefaultConfig()
+  config := consulapi.DefaultConfig()
   config.Address = consulAddr
-  client, err := api.NewClient(config)
+  client, err := consulapi.NewClient(config)
   if err != nil {
       log.Fatalf("Error creating Consul client: %v", err)
   }
 
   // Define the health check for the service
-  healthCheck := &api.AgentServiceCheck{
-      HTTP:                           fmt.Sprintf("http://%s:%s/ping", serviceIP, servicePort),
+  healthCheck := &consulapi.AgentServiceCheck{
+      HTTP:                           fmt.Sprintf("http://%s:%s/ping", serviceIP, s.port),
       Interval:                       "10s",
       Timeout:                        "5s",
       DeregisterCriticalServiceAfter: "1m",
   }
 
   // Register the service with Consul
-  serviceRegistration := &api.AgentServiceRegistration{
-      ID:      serviceID,
-      Name:    serviceName,
+  serviceRegistration := &consulapi.AgentServiceRegistration{
+      ID:      s.id,
+      Name:    s.name,
       Tags:    serviceTags,
       Address: serviceIP, // The address of your Golang service
-      Port:    atoi(servicePort), // The port your service listens on
+      Port:    atoi(s.port), // The port your service listens on
       Check:   healthCheck, // Health check for the service
   }
 
@@ -80,50 +76,8 @@ func registerServiceWithConsul(serviceID string) {
   if err != nil {
       log.Fatalf("Error registering service with Consul: %v", err)
   }
-  log.Printf("Service %s registered with Consul", serviceName)
+  log.Printf("Service %s registered with Consul", s.name)
 }
-
-// func registerService() {
-//   consulAddr := os.Getenv("CONSUL_HTTP_ADDR")
-//   serviceName := os.Getenv("SERVICE_NAME")
-//   servicePort := os.Getenv("SERVICE_PORT")
-
-//   hostname, _ := os.Hostname()  // Get container ID for unique registration
-
-//   serviceID := fmt.Sprintf("%s-%s", serviceName, hostname)
-//   serviceIP := os.Getenv("SERVICE_IP")  // Optionally pass if known, or discover dynamically
-
-//   if serviceIP == "" {
-//       serviceIP = getOutboundIP()  // Get container IP dynamically
-//   }
-
-//   payload := ConsulService{
-//       ID:      serviceID,
-//       Name:    serviceName,
-//       Tags:    []string{"go-service"},
-//       Address: serviceIP,
-//       Port:    atoi(servicePort),
-//       Check: map[string]string{
-//           "http": fmt.Sprintf("http://%s:%s/ping", serviceIP, servicePort),
-//           "interval": "30s",
-//           "timeout": "5s",
-//           "deregistercriticalserviceafter": "1m",
-//       },
-//   }
-
-//   data, _ := json.Marshal(payload)
-
-//   req, err := http.NewRequest("PUT", fmt.Sprintf("%s/v1/agent/service/register", consulAddr), bytes.NewBuffer(data))
-//   req.Header.Set("Content-Type", "application/json")
-//   resp, err := http.DefaultClient.Do(req)
-//   if err != nil {
-//       fmt.Printf("Failed to register service: %s\n", err)
-//       return
-//   }
-//   defer resp.Body.Close()
-
-//   fmt.Println("Service registered with Consul")
-// }
 
 func getOutboundIP() string {
   conn, _ := net.Dial("udp", "8.8.8.8:80")
@@ -133,20 +87,42 @@ func getOutboundIP() string {
 }
 
 func atoi(str string) int {
-  i, _ := strconv.Atoi(str)
+  i, err := strconv.Atoi(str)
+  if err != nil {
+    return 18080
+  }
   return i
 }
 
+type service struct {
+  name string
+  id string
+  port string
+}
+
+func getService() *service {
+  serviceName := os.Getenv("SERVICE_NAME")
+  serviceID := getServiceID()
+  servicePort := os.Getenv("SERVICE_PORT")
+
+  s := service{
+    name: serviceName,
+    id: serviceID,
+    port: servicePort,
+  }
+  return &s
+}
+
 func main() {
-    serviceID := getServiceID()
-    registerServiceWithConsul(serviceID)
+    s := getService()
+    registerServiceWithConsul(s)
 
     // Handle graceful shutdown
     sigs := make(chan os.Signal, 1)
     signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
     go func() {
         <-sigs
-        deregisterServiceWithConsul(serviceID)
+        deregisterServiceWithConsul(s.id)
     }()
 
     rand.Seed(time.Now().UnixNano())
@@ -158,29 +134,19 @@ func main() {
     // r := gin.New()
     r := gin.Default()
 
-    // r.GET("/health", func(c *gin.Context) {
-    //   c.JSON(http.StatusOK, gin.H{"status": "healthy"})
-    // })
-
-    // Define the /ping route
-    // r.GET("/ping", func(c *gin.Context) {
-    //     c.String(200, "Pong!")
-    // })
     r.GET("/ping", func(c *gin.Context) {
         c.JSON(http.StatusOK, gin.H{
           "message": "Pong!",
+          "serviceID": s.id,
         })
       })
 
-    // Define the /pong route
-    // r.GET("/pong", func(c *gin.Context) {
-    //     c.String(200, "Ping!")
-    // })
     r.GET("/pong", func(c *gin.Context) {
         switch rand.Intn(2) {
         case 0:
             c.JSON(http.StatusOK, gin.H{
             "message": "Ping!",
+            "serviceID": s.id,
             })
         case 1:
             c.JSON(http.StatusInternalServerError, gin.H{
